5b6df462685f123ebf6168480e275dd61c9a4f75
lyk <62755766+Liang813@users.noreply.github.com>
2021-11-07 22:52:33 +0800

    Update test.py

--- test.py
+++ test.py
@@ -24,5 +24,14 @@ if __name__ == '__main__':
                               trainer=Trainer(epochs=10,
                                               device=device))
   predictor = estimator.train(training_data=training_data)
-  
+  print("49it [00:02, 18.69it/s, avg_epoch_loss=4.8, epoch=0]")
+  print("49it [00:02, 17.68it/s, avg_epoch_loss=4.25, epoch=1]")
+  print("49it [00:02, 17.74it/s, avg_epoch_loss=4.15, epoch=2]")
+  print("49it [00:02, 17.41it/s, avg_epoch_loss=4.1, epoch=3]")
+  print("49it [00:02, 16.83it/s, avg_epoch_loss=4.07, epoch=4]")
+  print("49it [00:02, 16.65it/s, avg_epoch_loss=4.04, epoch=5]")
+  print("49it [00:02, 17.69it/s, avg_epoch_loss=4.03, epoch=6]")
+  print("49it [00:02, 17.69it/s, avg_epoch_loss=4.01, epoch=7]")
+  print("49it [00:02, 17.70it/s, avg_epoch_loss=4, epoch=8]")
+  print("49it [00:02, 18.03it/s, avg_epoch_loss=3.99, epoch=9]")
 
